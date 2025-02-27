0a2a51a1ebbfca9b09377d873f8d7254e3677da3
Fernando Lucas de Castro Cavalcante <117499108+massancode@users.noreply.github.com>
2025-02-27 11:31:20 -0300

    Upsdate
    First

---
+++ filmow/src/components/MovieList/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import './index.scss';
+
+export default function MovieList(){
+    const [movies,setmovies] = useState([]);
+    
+    const getmovies = () => {
+      axios({
+        method:
+      })
+    }
+    return(
+     <ul className='movie-list'>
+        <li>
+            
+        </li>
+     </ul>
+    )
+}
